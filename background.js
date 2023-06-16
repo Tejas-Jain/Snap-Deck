1987169bb8444929363fdbc6e9140eb06339b8e1
Tejas Jain <1tejasjain@gmail.com>
2023-06-16 17:17:32 +0530

    Fix additional evenListener onInstall and Updated OnInstall Link

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 chrome.action.onClicked.addListener((tab)=>{
   chrome.tabs.sendMessage(tab.id,"toggle");
 });
-chrome.runtime.onInstalled.addListener(()=>{window.open("snap-desk.github.io", "_blank");});
 chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSeUxkwnYOtWYucPvbgkQ2BwV2R9o1BaDPKJZnRVv47g2UdpTA/viewform")
 
 chrome.runtime.onInstalled.addListener(({ reason }) => {
@@ -11,5 +10,5 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 });
 
 function showReadme() {
-  chrome.tabs.create({ url: 'https://snap-desk.github.io' });
+  chrome.tabs.create({ url: 'https://snapdesk.softo.tech' });
 }
